3f1c4aae601d067e526c0a693538e5a0a472dcb3
Chih-Wei,Hsu <f20020303@gmail.com>
2025-05-11 16:56:31 +0800

    update

--- FunctionalDescribe.py
+++ FunctionalDescribe.py
@@ -26,4 +26,26 @@ def describe(df):
         
     return output
 
+class color:
+   BLUE = '\033[94m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
+
+#Output of Describes
+print(color.BOLD + color.BLUE + color.UNDERLINE +
+      '"The describe table of df1 : Mental illness dataframe"' + color.END)
+print(describe(df1))
+print("\n")
+print(color.BOLD + color.BLUE + color.UNDERLINE +
+      '"The describe table of df2 : Adult population, mental illnesses"' + color.END)
+print(describe(df2))
+print("\n")
+print(color.BOLD + color.BLUE + color.UNDERLINE +
+      '"The describe table of df3 : Depressive"' + color.END)
+print(describe(df3))
+print("\n")
+print(color.BOLD + color.BLUE + color.UNDERLINE +
+      '"The describe table of df4 : Number of countries"' + color.END)
+print(describe(df4))
--- calldata.py
+++ calldata.py
@@ -1,4 +1,20 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+from sklearn import metrics
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+from plotly.offline import init_notebook_mode
+init_notebook_mode(connected=True)
+import  warnings
+warnings.filterwarnings("ignore")
 
 Data1 = pd.read_csv("/Users/xuzhiwei/Mental Health (EDA + Prediction)/Dataset/Mental Illnesses Prevalence.csv")
 Data2 = pd.read_csv("/Users/xuzhiwei/Mental Health (EDA + Prediction)/Dataset/Mental Health Adult Population Data.csv")
@@ -11,4 +27,63 @@ df3 = pd.DataFrame(Data3)
 df4 = pd.DataFrame(Data4)
 
 
-print(df1)
+#print(df1)
+
+def describe(df):
+    
+    
+    variables = []
+    dtypes = []
+    count = []
+    unique = []
+    missing = []
+    
+    
+    for item in df.columns:
+        variables.append(item)
+        dtypes.append(df[item].dtype)
+        count.append(len(df[item]))
+        unique.append(len(df[item].unique()))
+        missing.append(df[item].isna().sum())
+        
+
+    output = pd.DataFrame({
+        'variable': variables, 
+        'dtype': dtypes,
+        'count': count,
+        'unique': unique,
+        'missing value': missing
+    })    
+        
+    return output
+
+class color:
+   BLUE = '\033[94m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+
+#Output of Describes
+print(color.BOLD + color.BLUE + color.UNDERLINE +
+      '"The describe table of df1 : Mental illness dataframe"' + color.END)
+print(describe(df1))
+print("\n")
+print(color.BOLD + color.BLUE + color.UNDERLINE +
+      '"The describe table of df2 : Adult population, mental illnesses"' + color.END)
+print(describe(df2))
+print("\n")
+print(color.BOLD + color.BLUE + color.UNDERLINE +
+      '"The describe table of df3 : Depressive"' + color.END)
+print(describe(df3))
+print("\n")
+print(color.BOLD + color.BLUE + color.UNDERLINE +
+      '"The describe table of df4 : Number of countries"' + color.END)
+print(describe(df4))
+
+#Some Visualizations with Plotly
+
+df2.sort_values(by= "Major depression" ,inplace=True)
+plt.figure(dpi=200)
+fig = px.bar(df2, x="Major depression", y="Entity", orientation='h',color='Bipolar disorder')
+fig.show()
