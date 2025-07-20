8dfca20c1d6e5ccb8ab7b1744d32498d3b7b61bb
Chih-Wei,Hsu <f20020303@gmail.com>
2025-07-20 14:13:30 +0800

    restart

--- calldata.py
+++ calldata.py
@@ -21,6 +21,7 @@ Data2 = pd.read_csv("/Users/xuzhiwei/Mental Health (EDA + Prediction)/Dataset/Me
 Data3 = pd.read_csv("/Users/xuzhiwei/Mental Health (EDA + Prediction)/Dataset/Depressive Symptoms US Population.csv")
 Data4 = pd.read_csv("/Users/xuzhiwei/Mental Health (EDA + Prediction)/Dataset/Mental Health Countries Data.csv")
 
+
 df1 = pd.DataFrame(Data1)
 df2 = pd.DataFrame(Data2)
 df3 = pd.DataFrame(Data3)
